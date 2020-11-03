8d603fb4272a484bb9f72ed7422a2d937886abc5
Storky <storky-bus@yandex.ru>
2020-11-03 16:35:19 +0300

    initial project version

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// todo redux devtools
+// todo reducer
+
+// todo provider and router
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
